5f5b3b99c0b369be7f79598e485a366297acb9ae
Jigar634859 <144481462+Jigar634859@users.noreply.github.com>
2025-06-18 02:18:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://newcode-algo-backend.onrender.com',
         secure: false,
         changeOrigin: true
       }
